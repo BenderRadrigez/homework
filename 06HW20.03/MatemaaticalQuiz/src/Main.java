886af7eb968fb126b7ef0982014adceb72f1ad92
Andrei Petrusevich <111006214+BenderRadrigez@users.noreply.github.com>
2023-04-08 01:57:48 +0200

    Rework all code
    Написать программу, которая:
    При запуске выводит приветственное сообщение, и просит пользователя пройти математический quiz.
    Последовательно выводит 5 вопросов разных типов(между двумя числами с операциями: +, -, *, /).
    После каждого вопроса ожидает ответа от пользователя.
    Если ответ правильный, выводит пользователю одобряющее сообщение.
    Если ответ неправильный, называет правильный ответ.
    По окончании 5 вопросов выводится сообщение об окончании игры.
    Вопросы и числа для вопросов генерируют случайным образом.
    Все вопросы задаются внутри цикла.
    
    УРОВЕНЬ 2.
    Ввести балльную систему.
    За каждый правильный ответ пользователю присуждается (+5) баллов.
    За каждый неправильный ответ отнимается (-5) балла.
    В самом конце выводится финальное количество баллов.
    
    УРОВЕНЬ 3.
    Ввести второго игрока.
    В начале игры обоих игроков просят ввести имена.
    Для каждого игрока ведётся свой подсчёт очков. 
    Каждому игроку задаётся по 5 вопросов. Задаются либо чередованием между первым и вторым игроком, либо сначала все 5 вопросов первому игроку, потом 5 второму.
    В конце игры выводятся имена игроков и их количество баллов.
    Имена должны быть выведены в порядке полученных баллов(тот, кто набрал больше - первым,   тот, кто меньше - вторым).

--- 06HW20.03/MatemaaticalQuiz/src/Main.java
+++ 06HW20.03/MatemaaticalQuiz/src/Main.java
@@ -2,66 +2,109 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    public static Random random = new Random();
+    public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        int firstNum;
-        int secondNum;
-        int result;
-        int answer;
-        int score;
-        int continueGame;
-        int randomChar;
+        int firstNum, secondNum;       //числа для математической операции
+        int result;         //результат математической операции
+        int answer;         //ответ пользователя
+        int randomChar;     //выбор символа для математической операции
+        int numberOfPlayers; //колличество игроков
+        int value = 5;      //очки которые добавляем или отнимаем у игроков
+        int numberOfQuestions = 5;  //кол-во вопросов
+
+        String[] arrayNameOfPlayers;    //массив из имен игроков
+        int[] arrayScoreOfPlayers;      //массив очков для каждого игрока
         char[] operator = {'+', '-', '*', '/', '%'};
 
-        printStartMessage();
-        scanner.nextLine();
+        printStartMessage();        //стартовое приветсвие
 
-        while(true){
-            firstNum = random.nextInt(50);
-            secondNum = random.nextInt(50);
-            randomChar = random.nextInt(5);
+        //получаем кол-во игроков и массив из имен
+        do {
+            arrayNameOfPlayers = howManyPlayersInGame();
+            numberOfPlayers = arrayNameOfPlayers.length;
+            arrayScoreOfPlayers = new int[numberOfPlayers];
+        } while (numberOfPlayers == 0);
 
-            result = matematicOperation(randomChar, firstNum, secondNum);
+        //процесс игры
+        for(int i = 0; i < numberOfQuestions; i++){
+            for(int j = 0; j < numberOfPlayers; j++){
+                System.out.println("Вопрос " + (i+1) + " для игрока " + arrayNameOfPlayers[j]);
+                firstNum = random.nextInt(50);
+                secondNum = random.nextInt(50);
+                randomChar = random.nextInt(5);
 
-            System.out.println(firstNum + " " + operator[randomChar] + " " + secondNum);
-            answer = scanner.nextInt();
+                result = matematicOperation(randomChar, firstNum, secondNum);
 
-            if (answer != result) {
-                System.out.println("ты проиграл, правильный ответ " + result + ". Но не расстраивайся ты -");
-                break;
+                System.out.print(firstNum + " " + operator[randomChar] + " " + secondNum + " = ");
+                answer = scanner.nextInt();
+
+                if (answer != result) {
+                    System.out.print("ответ " + answer + " не верный, правильный ответ " + result + ".");
+                    arrayScoreOfPlayers[j] -= value;
+                } else {
+                    System.out.print("правильный ответ, ты заработал +5 очков");
+                    arrayScoreOfPlayers[j] += value;
+                }
+                System.out.println(" у тебя уже " + arrayScoreOfPlayers[j] + " очков");
             }
-
-            score++;
-
-            System.out.println("правильный ответ, ты заработал +1 очко, твой счет составляет: " + score);
-            System.out.println("чтобы продолжить выбери 1 чтобы завершить нажми 0");
-            continueGame = scanner.nextInt();
-            if (continueGame == 0) break;
         }
-
-        printFinalMessage(score);
+        //завершение игры
+        whoIsWin(arrayNameOfPlayers, arrayScoreOfPlayers);
     }
 
     static int matematicOperation(int randomOperatorNumber, int num1, int num2){
-        if (randomOperatorNumber == 0) return num1 + num2;
-        else if (randomOperatorNumber == 1) return num1 - num2;
-        else if (randomOperatorNumber == 2) return num1 * num2;
-        else if (randomOperatorNumber == 3) return num1 / num2;
-        return num1 % num2;
+        switch (randomOperatorNumber) {
+            case 0: return num1 + num2;
+            case 1: return num1 - num2;
+            case 2: return num1 * num2;
+            case 3: return num1 / num2;
+            default: return num1 % num2;
+        }
+    }
+
+    static String[] howManyPlayersInGame(){
+        System.out.println("Скажите сколько игроков хочет поучаствовать в игре?");
+        String[] arrayPlayers = new String[scanner.nextInt()];
+        for(int i = 0; i < arrayPlayers.length; i++){
+            System.out.println("Введите имя " + (i+1) + "-го игрока:");
+            arrayPlayers[i] = scanner.next();
+        }
+        return arrayPlayers;
     }
 
     static void printStartMessage(){
-        System.out.println("Привет. я здесь, чтобы сыграть с тобой в игру...");
-        System.out.println("Правила просты, я даю тебе математический пример('+,-,*,/,%') - ты отвечаешь.");
-        System.out.println("Учти что результат деления нужно указать без остатка! только целое число(округление в меньшую сторону)!");
-        System.out.println("Если ты ответишь не правильно ты проиграл. начнем?");
+        System.out.println("Привет. вы здесь, чтобы сыграть в игру...");
+        System.out.println("Правила просты, я даю математический пример('+,-,*,/,%') - вы отвечаете по очереди.");
+        System.out.println("Учтите что результат деления нужно указать без остатка! только целое число(округление в меньшую сторону)!");
+        System.out.println("За каждый правильный ответ участник получает 5 баллов, если ответ не правильный он их теряет. начнем?");
         System.out.println("нажми enter чтоб продолжить...");
+        scanner.nextLine();
     }
 
-    static void printFinalMessage(int score){
-        if (score == 1) System.out.println("молодец, даже самый маленький успех - все равно успех! ты заработал " + score + " очко.)");
-        else if (score >= 2 && score <= 4 ) System.out.println("молодец, даже самый маленький успех - все равно успех! ты заработал " + score + " очка.)");
-        else System.out.println("молодец, даже самый маленький успех - все равно успех! ты заработал " + score + " очков.)");
+    static void whoIsWin(String[] arrayNames, int[] arrayScores){
+        String someString;
+        int someValue, index;
+
+        for(int i = 0; i < arrayScores.length; i++){
+            index = i;
+            for (int j = i; j < arrayScores.length; j++){
+                if (arrayScores[j] > arrayScores[i]){
+                    index = j;
+                }
+            }
+            someValue = arrayScores[i];
+            arrayScores[i] = arrayScores[index];
+            arrayScores[index] = someValue;
+
+            someString = arrayNames[i];
+            arrayNames[i] = arrayNames[index];
+            arrayNames[index] = someString;
+        }
+        System.out.println();
+
+        for (int i = 0; i < arrayNames.length; i++){
+            System.out.println((i+1) + " место занимает " + arrayNames[i] + "! Набрав " + arrayScores[i] + " очков.");
+        }
     }
 }
