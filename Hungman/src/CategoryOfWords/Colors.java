c94dfd0c070a0572d29417d8b83aa1e77ab277f3
Andrei Petrusevich <111006214+BenderRadrigez@users.noreply.github.com>
2023-04-15 21:57:27 +0200

    StartPetProject
    1 все слова - делились на категории
    2взять рандомное слово
    3метод который скрывает слово
    4метод который при угадывании буквы показывает ее в скрытом слове
    5метод если игрок не угадывает букву выводи в консоль виселицу (с каждой не угаданной буквой добавляем к висилице элемент)
    6метод проверки на победу
    7уровень сложности

---
+++ Hungman/src/CategoryOfWords/Colors.java
@@ -0,0 +1,5 @@
+package CategoryOfWords;
+
+public class Colors {
+    public static String[] words = {"красный", "оранжевый", "желтый", "зеленый", "синий", "голубой", "фиолетовый", "белый", "черный", "коричневый"};
+}
---
+++ Hungman/src/CategoryOfWords/Fruits.java
@@ -0,0 +1,4 @@
+package CategoryOfWords;
+
+public class Fruits {
+}
