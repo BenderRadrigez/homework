9cacec15694d8e0541559e6abf00d0dad5d33ac8
Andrei Petrusevich <111006214+BenderRadrigez@users.noreply.github.com>
2023-04-16 02:26:59 +0200

    Update Fruits.java

--- Hungman/src/CategoryOfWords/Fruits.java
+++ Hungman/src/CategoryOfWords/Fruits.java
@@ -1,4 +1,10 @@
 package CategoryOfWords;
 
+import java.util.Random;
+
 public class Fruits {
+    String[] words = {"яблоко", "апельсин", "лимон", "лайм", "груша", "мандарин", "виноград",
+            "грейпфрут", "персик", "банан", "абрикос", "манго", "нектарин"};
+
+    public String word = words[new Random().nextInt(words.length)];
 }
