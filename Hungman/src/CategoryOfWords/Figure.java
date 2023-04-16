5e731474c8a2a45671ecf4980abd0e23e95908c4
Andrei Petrusevich <111006214+BenderRadrigez@users.noreply.github.com>
2023-04-16 02:26:35 +0200

    Update Figure.java

--- Hungman/src/CategoryOfWords/Figure.java
+++ Hungman/src/CategoryOfWords/Figure.java
@@ -1,4 +1,10 @@
 package CategoryOfWords;
 
+import java.util.Random;
+
 public class Figure {
+    String[] words = {"квадрат", "треугольник", "прямоугольник", "круг", "эллипс", "ромб","трапеция",
+            "параллелограмм", "пятиугольник", "шестиугольник", "восьмиугольник"};
+
+    public String word = words[new Random().nextInt(words.length)];
 }
